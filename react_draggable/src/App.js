63f93d92b9627e4e3a19ac08504c95f2651c4ab5
seeys <es2008e@naver.com>
2022-05-31 23:55:33 +0900

    update react_drag App

--- react_draggable/src/App.js
+++ react_draggable/src/App.js
@@ -1,5 +1,21 @@
+import { useRef } from "react";
+import Draggable from "./lib/Draggable";
+
 function App() {
-  return <></>;
+  const handle = useRef(null);
+  return (
+    <Draggable handleRef={handle} onMove={(x, y) => console.log(x, y)}>
+      <div
+        style={{
+          width: "100px",
+          height: "100px",
+          backgroundColor: "greenyellow",
+        }}
+      >
+        <button ref={handle}>Move</button>
+      </div>
+    </Draggable>
+  );
 }
 
 export default App;
