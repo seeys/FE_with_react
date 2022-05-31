36c9ae6276b78c5df5a3e012e4ad558133a8ed7d
seeys <es2008e@naver.com>
2022-05-31 21:03:19 +0900

    add js_sortable

---
+++ js_sortable/src/TestItem/TestData.js
@@ -0,0 +1,17 @@
+export const data = [
+  {
+    content: "react",
+  },
+  {
+    content: "raaa",
+  },
+  {
+    content: "rebga",
+  },
+  {
+    content: "rsdfa",
+  },
+  {
+    content: "nbbnxc",
+  },
+];
