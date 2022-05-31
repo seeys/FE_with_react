a83a1c9c5c5d372338711ce3f5f71c399f7ce7be
seeys <es2008e@naver.com>
2022-05-31 20:59:10 +0900

    add js_musicplayer

---
+++ js_musicplayer/src/App.js
@@ -0,0 +1,45 @@
+import "./App.scss";
+import SongDetail from "./components/SongDetail/SongDetail";
+import ProgressArea from "./components/ProgressArea/ProgressArea";
+import Controls from "./components/Controls/Controls";
+import PlayList from "./components/PlayList/PlayList";
+import React, { useCallback, useRef, useState } from "react";
+
+function App() {
+  const audioRef = useRef();
+  const [showPlayList, setshowPlayList] = useState(false);
+
+  const onPlay = useCallback(() => {
+    audioRef.current.play();
+  }, []);
+  const onPause = useCallback(() => {
+    audioRef.current.pause();
+  }, []);
+  const changeVolume = useCallback((volume) => {
+    audioRef.current.changeVolume(volume);
+  }, []);
+  const resetDuration = useCallback(() => {
+    audioRef.current.resetDuration();
+  }, []);
+  return (
+    <div className="App">
+      <div className="container">
+        <SongDetail />
+        <ProgressArea ref={audioRef} />
+        <Controls
+          setshowPlayList={setshowPlayList}
+          play={onPlay}
+          pause={onPause}
+          changeVolume={changeVolume}
+          resetDuration={resetDuration}
+        />
+        <PlayList
+          setshowPlayList={setshowPlayList}
+          showPlayList={showPlayList}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ js_musicplayer/src/components/PlayList/PlayList.jsx
@@ -0,0 +1,64 @@
+import React, { memo, useCallback } from "react";
+import QueueMusic from "@mui/icons-material/QueueMusic";
+import Close from "@mui/icons-material/Close";
+import PlayListItem from "./PlayListItem";
+import classNames from "classnames";
+
+import "./PlayList.scss";
+import SortableList from "@billy-fe/sortable-list";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  setCurrentIndex,
+  updatePlayList,
+} from "../../store/musicPlayerReducer";
+
+const PlayList = ({ showPlayList, setshowPlayList }) => {
+  const playList = useSelector((state) => state.playList);
+  const dispatch = useDispatch();
+
+  const onClickClosePlayList = useCallback(() => {
+    setshowPlayList(false);
+  }, [setshowPlayList]);
+
+  const onClickItem = useCallback(
+    (index) => {
+      dispatch(setCurrentIndex(index));
+    },
+    [dispatch]
+  );
+
+  const onDropItem = useCallback(
+    (newPlayList) => {
+      dispatch(updatePlayList(newPlayList));
+    },
+    [dispatch]
+  );
+
+  const renderItem = useCallback(
+    (item, index) => <PlayListItem item={item} index={index} />,
+    []
+  );
+
+  return (
+    <div className={classNames("play-list", { show: showPlayList })}>
+      <div className="header">
+        <div className="row">
+          <QueueMusic className="list" />
+          <span>Play list</span>
+        </div>
+        <Close
+          sx={{ fontSize: 22, cursor: "pointer" }}
+          onClick={onClickClosePlayList}
+        />
+      </div>
+      <SortableList
+        date={playList}
+        renderItem={renderItem}
+        onDropItem={onDropItem}
+        onClickItem={onClickItem}
+      />
+    </div>
+  );
+};
+
+export default memo(PlayList);
