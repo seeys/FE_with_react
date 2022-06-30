3c3aaa4edfc7a2af6ea9a3d5671047842a65d5ac
seeys <es2008e@naver.com>
2022-06-30 22:35:55 +0900

    update react_carousel.tsx

--- react_carousel/src/App.tsx
+++ react_carousel/src/App.tsx
@@ -1,17 +1,33 @@
 import React from "react";
-import Antd from "./components/Antd";
-import Responsive from "./components/Responsive";
-import Slick from "./components/Slick";
+import Carousel from "./components/Carousel";
 
 function App() {
   return (
     <div>
-      <div>Antd</div>
-      <Antd />
-      <div>Responsive</div>
-      <Responsive />
-      <div>Slick</div>
-      <Slick />
+      <Carousel
+        loop
+        autoLoop
+        autoTime={3000}
+        transitionTime={1000}
+        direction="row"
+      >
+        <h1>hello</h1>
+        <h2>world</h2>
+        <h3>react</h3>
+        <h4>fastcampus</h4>
+      </Carousel>
+      {/* <div>
+			  Antd
+		  </div>
+		  <Antd />
+		  <div>
+			  Responsive
+		  </div>
+		  <Responsive />
+		  <div>
+			  Slick
+		  </div>
+		  <Slick /> */}
     </div>
   );
 }
---
+++ react_carousel/src/components/Carousel.tsx
@@ -0,0 +1,129 @@
+import styled from "@emotion/styled";
+import { ReactNode, useEffect, useState } from "react";
+const CarouselContainer = styled.div<{
+  direction: "column" | "row";
+}>`
+  width: 500px;
+  height: 500px;
+  background-color: #eee;
+  display: flex;
+  overflow: hidden;
+  position: relative;
+  flex-direction: ${({ direction }) => direction};
+`;
+const CarouselItem = styled.div<{
+  offset: number;
+  transitionTime: number;
+  direction: "column" | "row";
+}>`
+  width: 500px;
+  height: 500px;
+  min-width: 500px;
+  min-height: 500px;
+  transition: transform ${({ transitionTime }) => transitionTime}ms ease-in-out;
+  transform: translate ${({ direction }) => (direction === "row" ? "X" : "Y")}
+    (${({ offset }) => -offset * 100}%);
+`;
+const CarouselButton = styled.div<{
+  position: "left" | "right";
+  direction: "column" | "row";
+}>`
+  z-index: 999;
+  cursor: pointer;
+  width: 50px;
+  height: 50px;
+  background-color: #555;
+  color: white;
+  position: absolute;
+  font-size: 24px;
+  top: calc(
+    ${({ direction, position }) =>
+      direction === "row"
+        ? "50% - 25px"
+        : position === "left"
+        ? "0"
+        : "100% - 50px"}
+  );
+  ${({ position, direction }) =>
+    position === "left" &&
+    `left:${direction === "row" ? 0 : "calc(50% - 25px)"}`};
+  ${({ position, direction }) =>
+    position === "right" &&
+    `right:${direction === "row" ? 0 : "calc(50% - 25px)"}`};
+  ${({ direction }) => direction === "column" && "transform: rotate(90deg)"};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+interface CarouselProps {
+  children: ReactNode | ReactNode[];
+  loop?: boolean;
+  autoLoop?: boolean;
+  autoTime?: number;
+  transitionTime?: number;
+  direction?: "column" | "row";
+}
+const Carousel = ({
+  children: propsChildren,
+  loop,
+  direction = "row",
+  autoLoop,
+  autoTime = 3000,
+  transitionTime = 500,
+}: CarouselProps) => {
+  const children = Array.isArray(propsChildren)
+    ? propsChildren
+    : [propsChildren];
+  const [idx, setIdx] = useState(0);
+
+  useEffect(() => {
+    if (autoLoop) {
+      const intv = setInterval(
+        () => setIdx((prev) => (prev < children.length - 1 ? prev + 1 : 0)),
+        autoTime
+      );
+
+      return () => clearInterval(intv);
+    }
+  }, [autoLoop, autoTime, children.length]);
+
+  return (
+    <CarouselContainer direction={direction}>
+      <CarouselButton
+        direction={direction}
+        onClick={() => {
+          if (idx > 0) setIdx((prev) => prev - 1);
+          else if (loop) {
+            setIdx(children.length - 1);
+          }
+        }}
+        position="left"
+      >
+        {"<"}
+      </CarouselButton>
+      {children.map((child, index) => (
+        <CarouselItem
+          direction={direction}
+          transitionTime={transitionTime}
+          offset={idx}
+          key={index}
+        >
+          {child}
+        </CarouselItem>
+      ))}
+      <CarouselButton
+        direction={direction}
+        onClick={() => {
+          if (idx < children.length - 1) setIdx((prev) => prev + 1);
+          else if (loop) {
+            setIdx(0);
+          }
+        }}
+        position="right"
+      >
+        {">"}
+      </CarouselButton>
+    </CarouselContainer>
+  );
+};
+export default Carousel;
